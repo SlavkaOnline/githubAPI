0f1b3129c6ed179e5d0c2c7a3f91da535ac959f8
Vyacheslav Lopaev <vlopaev@knoema.com>
2022-01-19 18:40:21 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
 
-// https://api.github.com/users/SlavkaOnline/repos
 function App() {
 
     const [username, setUsername] = useState("");
@@ -10,34 +8,74 @@ function App() {
     const [reposContent, setReposContent] = useState([]);
     const [fileContent, setFileContent] = useState(null);
     const [dirContent, setDirContent] = useState(null);
+    const [videoContent, setVideoContent] = useState(null);
 
+    function clear() {
+        setFileContent(null);
+        setDirContent(null);
+        setVideoContent(null);
+    }
+
+    function sendRequest(url) {
+        return fetch('https://localhost:7268/api/github', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify({
+                url: url,
+                method: "GET",
+                payload: ""
+            })
+        }).then(res => res.json())
+    }
+
+    function sendVideoRequest(url) {
+        return fetch('https://localhost:7268/api/github', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify({
+                url: url,
+                method: "GET",
+                payload: ""
+            })
+        }).then(res => res.blob())
+    }
+
+    async function videoStream(url) {
+        setVideoContent(URL.createObjectURL(await sendVideoRequest(url)));
+    }
 
     function getRepos(e) {
         e.preventDefault()
-        fetch(`https://api.github.com/users/${username}/repos?per_page=100`)
-            .then(res => res.json())
+        clear();
+        sendRequest(`https://api.github.com/users/${username}/repos?type='all'`)
             .then(arr => setRepos(arr))
     }
 
     function getReposContent(url) {
-        setFileContent(null);
-        setDirContent(null);
-        fetch(`${url}/contents`)
-            .then(res => res.json())
+        clear();
+        sendRequest(`${url}/contents`)
             .then(rep => setReposContent(rep))
     }
 
     function getReview(r) {
-        setFileContent(null);
-        setDirContent(null);
+        clear();
         if (r.type === 'file') {
-            fetch(r.url)
-                .then(res => res.json())
-                .then(rep => setFileContent(atob(rep.content)))
+            const arr = r.name.split('.');
+            if (arr[arr.length - 1] === "mp4") {
+                videoStream(r.download_url);
+            } else {
+                sendRequest(r.url)
+                    .then(rep => setFileContent(atob(rep.content)))
+            }
         }
         if (r.type === 'dir') {
-            fetch(r.url)
-                .then(res => res.json())
+            sendRequest(r.url)
                 .then(dir => setDirContent(dir))
         }
     }
@@ -67,16 +105,24 @@ function App() {
             </div>
             <h3>View</h3>
             <div>
-            {
-                fileContent !== null
-                    ? (<div>{fileContent}</div>) : <></>
-            }
-            {
-                dirContent !== null
-                    ? (<ul>
-                        {dirContent.map(r => <li key={r.sha} className="rep" onClick={() => getReview(r)}>{r.name}</li>)}
-                    </ul>) : <></>
-            }
+                {
+                    fileContent !== null
+                        ? (<div>{fileContent}</div>) : <></>
+                }
+                {
+                    dirContent !== null
+                        ? (<ul>
+                            {dirContent.map(r => <li key={r.sha} className="rep"
+                                                     onClick={() => getReview(r)}>{r.name}</li>)}
+                        </ul>) : <></>
+                }
+                {
+                    videoContent !== null
+                        ? (<video controls width="500" height="500">
+                                <source src={videoContent}/>
+                            </video>)
+                        : <></>
+                }
             </div>
 
         </div>
